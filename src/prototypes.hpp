5e53a0c5cea184277f60f015c6266a636dc455ef
FuexFollets <fuex9follets@gmail.com>
2022-09-13 13:56:12 +0000

    Added final prototypes

--- src/prototypes.hpp
+++ src/prototypes.hpp
@@ -1,13 +1,33 @@
 #include <cstdint>
 #include <string>
-#include <tuple>
 #include <iostream>
 
 namespace sim {
     struct simstring {
-        struct iterator;
-        struct split_iterator;
-        struct find_iterator;
+        struct iterator {
+            iterator& operator++();
+            iterator& operator--();
+
+            char& operator*();
+
+            template <typename T_int> iterator operator+(T_int);
+            template <typename T_int> iterator operator-(T_int);
+
+            bool&& operator==(iterator);
+            bool&& operator!=(iterator);
+        };
+
+        struct split_ref_iterator : public iterator {
+            simstring& operator*();
+        };
+
+        struct split_iterator : public iterator {
+            simstring&& operator*();
+        };
+
+        strut find_iterator : public iterator {
+            std::size_t&& operator*();
+        };
 
         simstring();
         simstring(const char*);
@@ -19,18 +39,20 @@ namespace sim {
         simstring(const simstring&);
         simstring(const simstring&&);
 
-        template <typename Tv> char& operator[](Tv);
-        template <typename Ta> simstring&& operator+(Ta);
-        template <typename Ta> simstring& operator+=(Ta);
+        template <typename T_string> operator=(T_string);
+        template <typename T_int> char& operator[](T_int);
+        template <typename T_app> simstring&& operator+(T_app);
+        template <typename T_app> simstring& operator+=(T_app);
 
         template <typename T_split> iterator split(T_split);
+        template <typename T_split> iterator split_include(T_split);
         template <typename T_split> iterator split_ref(T_split);
+        template <typename T_split> iterator split_ref_include(T_split);
         template <typename T_find> std::size_t find(T_find);
         template <typename T_find> find_iterator find_all(T_find);
         template <typename T_start, typename T_end> simstring&& sub_string(T_start, T_end);
         template <typename T_start, typename T_end> simstring& sub_string_ref(T_start, T_end);
         template <typename T_first, typename T_second> simstring& swap(T_first, T_second);
-        
 
         iterator begin();
         iterator end();
@@ -41,6 +63,4 @@ namespace sim {
         friend std::ostream& operator<<(std::ostream&, simstring&);
         friend std::ostream& operator<<(std::ostream&, simstring&&);
     };
-
-
 }
