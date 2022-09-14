0a8c64afe355192e02de7563040a0e4b8a6a51c6
FuexFollets <fuex9follets@gmail.com>
2022-09-13 23:43:52 -0400

    Added simstring operators directory

---
+++ src/simstring/operators/operator_main.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "../prototypes.hpp"
+#include "../ss_initializers.hpp"
+
+namespace sim {
+    template <> void simstring::operator= <simstring> (simstring ss_string) {
+
+    }
+}
