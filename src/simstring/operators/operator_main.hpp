e76e992941eb92f569f2b514e79869528feaff10
FuexFollets <fuex9follets@gmail.com>
2022-09-14 19:05:50 +0000

    Added = operator

--- src/simstring/operators/operator_main.hpp
+++ src/simstring/operators/operator_main.hpp
@@ -1,10 +1,27 @@
 #pragma once
 
+#include <cstring>
+#include <cstdint>
+
 #include "../prototypes.hpp"
 #include "../ss_initializers.hpp"
 
 namespace sim {
-    template <> void simstring::operator= <simstring> (simstring ss_string) {
+    template <> void simstring::operator= <simstring> (const simstring ss_string) {
+        length = ss_string.size();
+        string_val = ( char* ) realloc(string_val, sizeof(char) * length);
+        std::memcpy(string_val, ss_string.string_val, length);
+    }
 
+    template <typename T_string> void simstring::operator= (const T_string str) {
+        simstring to_eq;
+        
+        try {
+            simstring to_eq{simstring(str)};
+        } catch {
+            // Put exception here
+        }
+        
+        *this = to_eq;
     }
 }
