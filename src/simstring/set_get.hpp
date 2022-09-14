f250808b68b1eb868447c7291423e87c34bd0be6
FuexFollets <fuex9follets@gmail.com>
2022-09-13 22:22:13 -0400

    Added getters/setters definitions

---
+++ src/simstring/set_get.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdint>
+#include "prototypes.hpp"
+
+std::size_t sim::simstring::len() {
+    return length;
+}
+
+std::size_t sim::simstring::size() {
+    return length;
+}
