29eb4efbba074fc04691d723ea66987b3c6c053e
FuexFollets <fuex9follets@gmail.com>
2022-09-19 11:49:28 +0000

    Deprecated src

--- src/libmain.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include "simstring/simstring_main.hpp"
--- src/simstring/simstring_main.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "prototypes.hpp"
-#include "set_get.hpp"
-#include "ss_initializers.hpp"
-#include "operators/operator_main.hpp"
-#include "conversions.hpp"
-#include "stdoutput.hpp"
-
