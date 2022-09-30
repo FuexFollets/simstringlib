8ee584c8e623943431eb38af7453e086b1d419d7
FuexFollets <fuex9follets@gmail.com>
2022-09-30 11:49:38 +0000

    Added utilities directory

---
+++ include/simstring/utils/incl.cpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../prototypes.hpp"
+#include "../initializers.hpp"
+#include "../setters_getters.hpp"
+#include "../operators/conversions.hpp"
+#include "../operators/std_operators.hpp"
+#include "../iterators/char_iterator.hpp"
+
