1de20e1d21f22d861249fbc25a589466db5d882c
FuexFollets <fuex9follets@gmail.com>
2022-10-07 14:55:06 +0000

    Added conversion from substring_operator to simstring

--- include/simstring/operators/conversions.hpp
+++ include/simstring/operators/conversions.hpp
@@ -9,9 +9,8 @@
 #include "../initializers.hpp"
 
 namespace sim {
-
     simstring::operator char* () {
-        char* new_str {(char*) calloc(length + 1, sizeof(char))};
+        char* new_str {reinterpret_cast<char*>(calloc(length + 1, sizeof(char)))};
         std::memcpy(new_str, string_val, length);
         *(new_str + length + 1) = (char) 0;
         return new_str;
@@ -19,13 +18,20 @@ namespace sim {
 
 
     simstring::operator const char* () {
-        return const_cast<const char*>((char*) (*this));
+        return const_cast<const char*>((char*)(*this));
     }
 
 
     simstring::operator std::string () {
-        return std::string((const char*) (*this));
+        return std::string((const char*)(*this));
     }
 
+	simstring substring_operator::as_simstring() {
+		return simstring(*base_string, substr_index_start, substr_index_end);
+	}
+
+	substring_operator::operator simstring () {
+		return this -> as_simstring();
+	}
 }
 
