d861a2349be565fa7ebfee72f498dbe6b06e51ba
FuexFollets <fuex9follets@gmail.com>
2022-10-07 10:57:22 -0400

    Sync Changes

--- include/simstring/simstring_operator/incl.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../prototypes.hpp"
-#include "../initializers.hpp"
-#include "../setters_getters.hpp"
-#include "../operators/conversions.hpp"
-#include "../operators/std_operators.hpp"
-#include "../iterators/char_iterator.hpp"
-
