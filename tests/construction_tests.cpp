d3c28f0001fa9c57608e73f46517ce12ce762f20
FuexFollets <fuex9follets@gmail.com>
2022-10-07 14:15:05 +0000

    Added substring_operator tests

--- tests/construction_tests.cpp
+++ tests/construction_tests.cpp
@@ -11,6 +11,13 @@ void from_cstring() {
 	std::cout << '\n';
 }
 
+void substring_operator() {
+	sim::simstring str("abcdefg");
+	
+	sim::substring_operator sel(str, 3, 5);
+}
+
 int main() {
 	from_cstring();
+	substring_operator();
 }
