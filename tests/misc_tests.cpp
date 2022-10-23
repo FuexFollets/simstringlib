84a46a6950e059796fee9256cdb1cc3da70dfa0b
FuexFollets <fuex9follets@gmail.com>
2022-10-23 17:34:30 -0400

    Improved Makefile

--- tests/misc_tests.cpp
+++ tests/misc_tests.cpp
@@ -4,6 +4,7 @@ void replace_test() {
     std::cout << "\n -- Replace test\n";
     sim::simstring hi("abcdefg");
     sim::simstring hi2("abcdefg");
+
     hi2.replace_substring(2, 4, sim::simstring("EE"));
     hi.replace_substring<sim::simstring>(2, 3, sim::simstring("HHHH"));
     std::cout << hi << '\n' << hi2 << "\n";
@@ -12,3 +13,4 @@ void replace_test() {
 int main() {
     replace_test();
 }
+
