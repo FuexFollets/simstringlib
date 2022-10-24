f9f394a2067da446b053e72f78b561439056f005
FuexFollets <fuex9follets@gmail.com>
2022-10-24 13:45:25 -0400

    Formatted initializer functions

--- include/simstring/initializers.hpp
+++ include/simstring/initializers.hpp
@@ -8,63 +8,72 @@
 #include "./operators/conversions.hpp"
 #include "./operators/std_operators.hpp"
 
+namespace sim {
 
-sim::simstring::simstring(const char* c_string) {
+simstring::simstring(const char* c_string) {
+    length = strlen(c_string);
+    string_val = reinterpret_cast<char*>(calloc(length, sizeof(char)));
+
+    std::memcpy(string_val, c_string, length);
+}
+
+/*
+simstring::simstring(const char* c_string) : 
+    length {strlen(c_string)},
+    string_val {reinterpret_cast<char*>(calloc(length, sizeof(char))} 
+            {
+
+            };
+*/
+
+
+simstring::simstring(const std::string&& std_string) {
+    const char* c_string {std_string.c_str()};
     length = {strlen(c_string)};
     string_val = reinterpret_cast<char*>(calloc(length, sizeof(char)));
 
     std::memcpy(string_val, c_string, length);
 }
 
-sim::simstring::simstring(const std::string&& std_string) {
-    const char* c_string{std_string.c_str()};
-    length = {strlen(c_string)};
+simstring::simstring(const std::string& std_string) {
+    const char* c_string {std_string.c_str()};
+    length = strlen(c_string);
     string_val = reinterpret_cast<char*>(calloc(length, sizeof(char)));
 
     std::memcpy(string_val, c_string, length);
 }
 
-sim::simstring::simstring(const std::string& std_string) {
-    const char* c_string{std_string.c_str()};
-    length = {strlen(c_string)};
-    string_val = reinterpret_cast<char*>(calloc(length, sizeof(char)));
-
-    std::memcpy(string_val, c_string, length);
-}
-
-sim::simstring::simstring(const char&& letter) {
+simstring::simstring(const char&& letter) {
     string_val = reinterpret_cast<char*>(calloc(1, sizeof(char)));
     string_val[0] = letter;
 }
 
-sim::simstring::simstring(const char& letter) {
+simstring::simstring(const char& letter) {
     string_val = reinterpret_cast<char*>(calloc(1, sizeof(char)));
     string_val[0] = letter;
 }
 
-sim::simstring::simstring(const sim::simstring&& ss_string) {
-    length = {ss_string.size()};
+simstring::simstring(const simstring&& ss_string) {
+    length = ss_string.size();
     string_val = reinterpret_cast<char*>(calloc(length, sizeof(char)));
 
     std::memcpy(string_val, ss_string.string_val, length);
 }
 
-sim::simstring::simstring(const sim::simstring& ss_string) {
-    length = {ss_string.size()};
+simstring::simstring(const simstring& ss_string) {
+    length = ss_string.size();
     string_val = reinterpret_cast<char*>(calloc(length, sizeof(char)));
 
     std::memcpy(string_val, ss_string.string_val, length);
 }
 
-sim::substring_operator::substring_operator
-	(simstring* str_ptr, std::size_t start, std::size_t end)
-		: base_string{str_ptr}, substr_index_start{start}, substr_index_end{end} {}
+substring_operator::substring_operator(simstring* str_ptr, std::size_t start, std::size_t end)
+    : base_string{str_ptr}, substr_index_start{start}, substr_index_end{end} {}
 
-sim::substring_operator::substring_operator
-	(simstring& str_ptr, std::size_t start, std::size_t end)
-		: base_string{&str_ptr}, substr_index_start{start}, substr_index_end{end} {}
+substring_operator::substring_operator(simstring& str_ptr, std::size_t start, std::size_t end)
+    : base_string{&str_ptr}, substr_index_start{start}, substr_index_end{end} {}
 
-sim::simstring::simstring(const simstring& str, std::size_t start, std::size_t end = 0) {
+simstring::simstring(const simstring& str, std::size_t start, std::size_t end = 0) {
 	std::size_t new_length {(end == 0) ? start : (end - start)};
 	std::size_t start_index {start * (end == 0)};
 
@@ -75,6 +84,8 @@ sim::simstring::simstring(const simstring& str, std::size_t start, std::size_t e
 }
 
 
-sim::simstring::~simstring() {
+simstring::~simstring() {
     this -> del_string_data();
 }
+
+} // End of namespcae sim
